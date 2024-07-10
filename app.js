58ee3f6b622c2ae4b5baa1e02fa27d7b10c87c0c
darelljay <darelljason1@gmail.com>
2024-07-10 14:11:36 +0900

    .

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const path = require('path');
 const helmet = require('helmet'); // Added for security
 const cors = require('cors'); // CORS module added
 require('dotenv').config(); // Added for environment variables
 
 app.use(helmet()); // Use helmet for security
 app.use(cors()); // Use CORS middleware
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 io.on('connection', (socket) => {
   console.log('사용자가 연결되었습니다.');
